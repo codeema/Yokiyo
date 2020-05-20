c46142480e3631672d256ebb926b308d93be1db5
codeema <emacode@yahoo.fr>
2020-05-20 18:27:01 +0300

    Register new models ontheadmin panel

--- funolympics/admin.py
+++ funolympics/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Venue, Profile
+from .models import Venue, Profile, Facility, Blog, Sport, Lesson
 
 # Register your models here.
 admin.site.register(Venue)
 admin.site.register(Profile)
+admin.site.register(Facility)
+admin.site.register(Blog)
+admin.site.register(Sport)
+admin.site.register(Lesson)
