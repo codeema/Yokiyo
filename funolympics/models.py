007c5644c2869befd8db72444c0f60174aa67fd2
codeema <emacode@yahoo.fr>
2020-05-20 18:03:00 +0300

    Add models that will create user profile on sign up

--- funolympics/models.py
+++ funolympics/models.py
@@ -21,3 +21,21 @@ class Venue(models.Model):
 
     def delete_venue(self):
         self.delete()
+
+#This is our model for user profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to='profilepic/')
+    currentLocation = models.TextField(max_length=500, blank=True)
+    email = models.EmailField(max_length=254)
+    venue = models.ForeignKey(Venue, null=True, blank=True, on_delete=models.CASCADE)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
