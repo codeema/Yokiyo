528297f4573ac916f3412237bb49dde3c0bbce77
mugendinjue <emacode@yahoo.fr>
2020-05-25 14:26:59 +0300

    remove schedule bug

--- funolympics/admin.py
+++ funolympics/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Venue, Profile, Facility, Blog, Sport, Lesson
+from .models import Venue, Profile, Facility, Blog, Sport, Lesson, schedule
 
 # Register your models here.
 admin.site.register(Venue)
@@ -8,3 +8,4 @@ admin.site.register(Facility)
 admin.site.register(Blog)
 admin.site.register(Sport)
 admin.site.register(Lesson)
+admin.site.register(schedule)
