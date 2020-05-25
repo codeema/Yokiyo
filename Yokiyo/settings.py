cd4dd624679530c674fd68ff15ab3b6a75eaae76
mugendinjue <emacode@yahoo.fr>
2020-05-25 17:15:00 +0300

    deployment 2

--- Yokiyo/settings.py
+++ Yokiyo/settings.py
@@ -128,8 +128,8 @@ STATICFILES_DIRS = (
 )
 
 #media
-MEDIA_URL = '/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
